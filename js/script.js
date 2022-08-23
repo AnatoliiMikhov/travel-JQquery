a1fe8947a7d1772ca5a787b443e638fe87722143
Anatolii Mikhov <anatoli.mihov@gmail.com>
2022-08-23 16:53:46 +0300

    modal overlay click close

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
 // Sensei version
-$(document).ready(function () {
+$( document ).ready( function () {
 
-	$('.main_btna, .main_btn, a[href="#sheldure"]').click(function () {
-		$('.overlay').fadeIn(800);
-		$('.modal').slideDown(800);
-	});
+    $( '.main_btna, .main_btn, a[href="#sheldure"]' ).click( function () {
+        $( '.overlay' ).fadeIn( 300 )
+        $( '.modal' ).slideDown( 300 )
+    } )
 
-	$('.close').click(function () {
-		$('.modal').slideUp(800);
-		$('.overlay').fadeOut(800);
-	});
+    $( '.close' ).click( function () {
+        $( '.modal' ).slideUp( 300 )
+        $( '.overlay' ).fadeOut( 300 )
+    } )
 
-});
+    $( '.overlay' ).click( function () {
+        $( '.modal' ).slideUp( 300 )
+        $( '.overlay' ).fadeOut( 300 )
+    } )
+
+} )
 // My version
 /* $(document).ready(function () {
-	$('.main_btna').on('click', function () {
-		$('.overlay').fadeIn('slow');
-		$('.modal').slideDown('slow');
-	});
+    $('.main_btna').on('click', function () {
+        $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+    });
 
-	$('.main_btn').on('click', function () {
-		$('.overlay').fadeIn('slow');
-		$('.modal').slideDown('slow');
-	});
+    $('.main_btn').on('click', function () {
+        $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+    });
 
-	$('.main_nav nav li:eq(1)').on('click', function (event) {
-		event.preventDefault();
-		$('.overlay').fadeIn('slow');
-		$('.modal').slideDown('slow');
-	});
+    $('.main_nav nav li:eq(1)').on('click', function (event) {
+        event.preventDefault();
+        $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+    });
 
-	$('.close').on('click', function () {
-		$('.overlay').fadeOut('slow');
-		$('.modal').slideUp('slow');
-	});
+    $('.close').on('click', function () {
+        $('.overlay').fadeOut('slow');
+        $('.modal').slideUp('slow');
+    });
 }); */
